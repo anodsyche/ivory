a96a372a9936765fc8c2edf502eeeaad15efe908
Obinna Anosike <obinna.anosike@windranger.io>
2022-09-23 13:55:44 -0400

    test

--- app/resources/request.py
+++
@@ -1,5 +0,0 @@
-from flask_restful import Resource
-
-class GoalRequest(Resource):
-    def get(self):
-        return {'hello': 'world'}
--- app/resources/session.py
+++
@@ -1,7 +0,0 @@
-from flask_restful import Resource
-
-class Session(Resource):
-    def get(self):
-        pass
-    def post(self):
-        pass
